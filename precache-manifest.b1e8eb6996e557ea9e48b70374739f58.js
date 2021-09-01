acc5d2e86d4dbd6261977111f344c81829fc1eb1
Yevhen Pecherskyi <gekape4erskii@gmail.com>
2021-09-01 18:29:05 +0300

    Updates

---
+++ precache-manifest.b1e8eb6996e557ea9e48b70374739f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352a7edd1fd1a899f481c09ae2aeb235",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8bc1fb04d82dbb742b78",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc1fb04d82dbb742b78",
+    "url": "./static/js/main.cc885ab0.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
